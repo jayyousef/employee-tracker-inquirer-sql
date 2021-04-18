567f67f0b6ee5c723bb4ea96806e49f03d99d22d
Jay Yousef <jay.yousef@gmail.com>
2021-04-18 14:40:55 -0500

    second commit with bare bones

--- employeeConnection.js
+++ employeeConnection.js
@@ -21,142 +21,217 @@ const connection = mysql.createConnection({
 const start = () => {
   inquirer
     .prompt({
-      name: 'postOrBid',
+      name: 'start',
       type: 'list',
-      message: 'Would you like to [POST] an auction or [BID] on an auction?',
-      choices: ['POST', 'BID', 'EXIT'],
+      message: 'What would you like to do?',
+      choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Add Department', 'Add Role', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'Exit'],
     })
     .then((answer) => {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid === 'POST') {
-        postAuction();
-      } else if (answer.postOrBid === 'BID') {
-        bidAuction();
+      // based on their answer, call the corresponding function
+      if (answer.start === 'View All Employees') {
+        viewAllEmployees();
+      } else if (answer.start === 'View All Employees By Department') {
+        employeesByDepartment();
+      } else if (answer.start === 'View All Employees By Manager') {
+        employeesByManager();
+      } else if (answer.start === 'Add Employee') {
+        addEmployee();
+      } else if (answer.start === 'Add Department') {
+        addDepartment();
+      } else if (answer.start === 'Add Role') {
+        addRole();
+      } else if (answer.start === 'Remove Employee') {
+        removeEmployee();
+      } else if (answer.start === 'Update Employee Role') {
+        updateEmployeeRole();
+      } else if (answer.start === 'Update Employee Manager') {
+        updateEmployeeManager();
       } else {
         connection.end();
       }
     });
 };
 
-// function to handle posting new items up for auction
-const postAuction = () => {
-  // prompt for info about the item being put up for auction
-  inquirer
-    .prompt([
-      {
-        name: 'item',
-        type: 'input',
-        message: 'What is the item you would like to submit?',
-      },
-      {
-        name: 'category',
-        type: 'input',
-        message: 'What category would you like to place your auction in?',
-      },
-      {
-        name: 'startingBid',
-        type: 'input',
-        message: 'What would you like your starting bid to be?',
-        validate(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-    ])
-    .then((answer) => {
-      // when finished prompting, insert a new item into the db with that info
-      connection.query(
-        'INSERT INTO auctions SET ?',
-        // QUESTION: What does the || 0 do?
-        {
-          item_name: answer.item,
-          category: answer.category,
-          starting_bid: answer.startingBid || 0,
-          highest_bid: answer.startingBid || 0,
-        },
-        (err) => {
-          if (err) throw err;
-          console.log('Your auction was created successfully!');
-          // re-prompt the user for if they want to bid or post
-          start();
-        }
-      );
-    });
-};
-
-const bidAuction = () => {
-  // query the database for all items being auctioned
-  connection.query('SELECT * FROM auctions', (err, results) => {
+const viewAllEmployees = () => {
+  connection.query('SELECT * FROM employee', (err, results) => {
     if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
-    inquirer
-      .prompt([
-        {
-          name: 'choice',
-          type: 'rawlist',
-          choices() {
-            const choiceArray = [];
-            results.forEach(({ item_name }) => {
-              choiceArray.push(item_name);
-            });
-            return choiceArray;
-          },
-          message: 'What auction would you like to place a bid in?',
-        },
-        {
-          name: 'bid',
-          type: 'input',
-          message: 'How much would you like to bid?',
-        },
-      ])
-      .then((answer) => {
-        // get the information of the chosen item
-        let chosenItem;
-        results.forEach((item) => {
-          if (item.item_name === answer.choice) {
-            chosenItem = item;
-          }
-        });
+    const employees = results;
+    console.table(employees)
+    setTimeout(returningPrompt, 1000);
+    setTimeout(start, 3000);
+  })
+}
 
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            'UPDATE auctions SET ? WHERE ?',
-            [
-              {
-                highest_bid: answer.bid,
-              },
-              {
-                id: chosenItem.id,
-              },
-            ],
-            (error) => {
-              if (error) throw err;
-              console.log('Bid placed successfully!');
-              start();
-            }
-          );
-        } else {
-          // bid wasn't high enough, so apologize and start over
-          console.log('Your bid was too low. Try again...');
-          start();
+const employeesByDepartment = () => {
+  let departments;
+  const getDepartments = () =>
+    new Promise((resolve, reject) => {
+      connection.query('SELECT departmentName FROM employee', (err, results) => {
+        if (err) reject(new Error('There was an error: ', err));
+        departments = JSON.parse(JSON.stringify(results))
+        resolve(departments)
+      })
+    })
+
+  getDepartments().then((departments) => {
+    //attempting to remove duplicates from an array
+    departments = Object.values(departments)
+    let uniqueDepartments = [...new Set(departments)];
+    console.log('this is uniqueDepartments', uniqueDepartments)
+    askDepartment(uniqueDepartments)
+  }).catch((err) => console.error('Promise rejected:', err))
+}
+
+const askDepartment = (departments) => {
+  inquirer
+    .prompt([{
+      name: 'department',
+      type: 'list',
+      message: 'Which department would you like to view?',
+      choices: departments
+    }]).then((answer) => {
+      connection.query('SELECT * FROM employee WHERE department_name=?', [answer.department], (err, results) => {
+        if (err) throw err;
+        console.log('this is results console log >>>', results);
+        console.log('below is the console table >>>');
+        console.table(results)
+      })
+    })
+}
+
+
+const employeesByManager = () => {
+  let managers;
+  const getManager = () =>
+    new Promise((resolve, reject) => {
+      connection.query('SELECT managerID FROM employee', (err, results) => {
+        if (err) reject(new Error('There was an error: ', err));
+        managers = JSON.parse(JSON.stringify(results))
+        resolve(managers)
+      })
+    })
+
+  getManager().then((managers) => {
+    //attempting to remove duplicates from an array
+    managers = Object.values(managers)
+    let uniqueManagers = [...new Set(managers)];
+    console.log('this is uniqueDepartments', uniqueManagers)
+    askManager(uniqueManagerss)
+  }).catch((err) => console.error('Promise rejected:', err))
+}
+
+const askManager = (managers) => {
+  inquirer
+    .prompt([{
+      name: 'manager',
+      type: 'list',
+      message: "Which Manager\'s employees would you like to view?",
+      choices: managers
+    }]).then((answer) => {
+      connection.query('SELECT * FROM employee WHERE managerID=?', [answer.manager], (err, results) => {
+        if (err) throw err;
+        console.log('this is results console log >>>', results);
+        console.log('below is the console table >>>');
+        console.table(results)
+      })
+    })
+}
+
+const removeEmployee = () => {
+  let employees;
+  //first we need to retrive the list of employees to use in the inquirer question
+  const getEmployees = () =>
+    new Promise((resolve, reject) =>
+      connection.query('SELECT first_name,last_name FROM employee', (err, results) => {
+        if (err) reject(new Error('There was an error: ', err));
+        employees = JSON.parse(JSON.stringify(results));
+        resolve(employees)
+      }))
+  getEmployees()
+    .then(() => {
+      askEmployee(employees)
+    })
+    .catch((err) => console.error('Promise rejected:', err))
+}
+
+const askEmployee = (employees) => {
+  //TODO need to take the object and turn it into an array of [first_name last_name]
+  //need to turn the employees into an array that can be read by inquirer
+  inquirer
+    .prompt([{
+      name: 'employee',
+      type: 'list',
+      message: 'Select the name of the employee you would like to delete:',
+      choices: employees
+    }]).then((answer) => {
+      //TODO: need to separate the values of the chosen employee into separate values to be inputted to SQL
+      connection.query(
+        'DELETE FROM employee WHERE first_name=? AND last_name=?',
+        [first_name, last_name],
+        (err, res) => {
+          if (err) throw err;
+          console.log(`Deleting ${res.affectedRows} from the database!\n`);
+          console.log(`Deleting ${first_name} ${last_name} from the Employee Database..\n`);
         }
-      });
-  });
-};
+      )
+      start()
+    })
+}
+
+
+const addEmployee = () => {
+  inquirer
+    .prompt([{
+      name: 'first_name',
+      type: 'input',
+      message: 'What is the FIRST name of the employee you would like to add?'
+    }, {
+      name: 'last_name',
+      type: 'input',
+      message: 'What is the LAST name of the employee you would like to add?'
+    }, {
+      name: 'department',
+      type: 'input',
+      message: 'What is this employees title?'
+    }, {
+      name: 'salary',
+      type: 'input',
+      message: 'What is this employees yearly salary? (ex. \"50000\")'
+    }]).then((answer) => {
+
+      //TODO: need to separate the values of the chosen employee into separate values to be inputted to SQL
+      connection.query(
+        `INSERT INTO employee (first_name, last_name, departmentName, roleID, manager_id) VALUES (${answer.first_name}, ${answer.last_name},${answer.department},${answer.salary})`,
+        (err, res) => {
+          if (err) throw err;
+          console.log(`Adding ${res.affectedRows} to the database!\n`);
+          console.log(`Adding ${first_name} ${last_name} to the Employee Database..\n`);
+        }
+      )
+      start()
+    }).catch((err) => console.error('Promise rejected:', err))
+}
+
 
 // connect to the mysql server and sql database
 connection.connect((err) => {
   if (err) throw err;
+  console.log('                        __                                                                    ')
+  console.log('  ___  ____ ___  ____  / /___  __  _____  ___     ____ ___  ____ _____  ____ _____ ____  _____')
+  console.log(' / _ \/ __ `__ \/ __ \/ / __ \/ / / / _ \/ _ \   / __ `__ \/ __ `/ __ \/ __ `/ __ `/ _ \/ ___/')
+  console.log('/  __/ / / / / / /_/ / / /_/ / /_/ /  __/  __/  / / / / / / /_/ / / / / /_/ / /_/ /  __/ /    ')
+  console.log('\___/_/ /_/ /_/ .___/_/\____/\__, /\___/\___/  /_/ /_/ /_/\__,_/_/ /_/\__,_/\__, /\___/_/     ')
+  console.log('             /_/            /____/                                         /____/             ')
   // run the start function after the connection is made to prompt the user
   start();
 });
 
-
-
+//this function tells the user that they're returning to the start 
+const returningPrompt = () => {
+  console.log(
+    'Returning to start...'
+  )
+}
 
 // console.table([
 //   {
--- employeeListDB.sql
+++ employeeListDB.sql
@@ -4,33 +4,33 @@ CREATE DATABASE employees_DB;
 USE employees_DB;
 
 CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT,
-  department_name VARCHAR(30),
-  PRIMARY KEY (id)
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT,
-  role_title VARCHAR(30) NOT NULL,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
   salary DECIMAL(2) NOT NULL,
-FOREIGN KEY (department_name) REFERENCES department(department_name)
-ON DELETE CASCADE,
-  PRIMARY KEY (id)
+  departmentName VARCHAR(30) NOT NULL REFERENCES department(name) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
-  item_name VARCHAR(100) NOT NULL,
-  category VARCHAR(45) NOT NULL,
-  starting_bid INT default 0,
-  highest_bid INT default 0,
-  PRIMARY KEY (id)
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name  VARCHAR(30) NOT NULL,
+  departmentName VARCHAR(30) NOT NULL REFERENCES department(name) ON DELETE CASCADE,
+  roleID INT NOT NULL REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT default 0
 );
 
-* **employee**:
 
-  * **id** - INT PRIMARY KEY
-  * **first_name** - VARCHAR(30) to hold employee first name
-  * **last_name** - VARCHAR(30) to hold employee last name
-  * **role_id** - INT to hold reference to role employee has
-  * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+INSERT INTO employee (first_name, last_name, departmentName, roleID, manager_id)
+VALUES ("vanilla", "johnson",'accounting',1, 100);
+
+INSERT INTO employee (first_name, last_name, departmentName, roleID, manager_id)
+VALUES ("test2", "test20",'marketing', 2, 200);
+
+INSERT INTO employee (first_name, last_name, departmentName, roleID, manager_id)
+VALUES ("etst3", "test30",'marketing',3, 300);
+
